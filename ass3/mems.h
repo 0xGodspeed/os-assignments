a05d5243a50a9ba53e0602e3ada08886a0c62d28
Arnav Jindal <26025037+arnavjindal@users.noreply.github.com>
2023-11-04 01:19:44 +0530

    mems_free() and mems_finish() done

--- ass3/mems.h
+++ ass3/mems.h
@@ -9,6 +9,7 @@ you can also make additional helper functions a you wish
 REFER DOCUMENTATION FOR MORE DETAILS ON FUNSTIONS AND THEIR FUNCTIONALITY
 */
 // add other headers as required
+#include <stdint.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,7 +24,7 @@ this macro to make the output of all system same and conduct a fair evaluation.
 #define PAGE_SIZE 4096
 #define HOLE 0
 #define PROCESS 1
-#define SIZE_MAX 100
+#define MAX_SIZE 1000
 
 struct main_node {
     int num_of_pages;
@@ -51,6 +52,10 @@ void* main_node_tracker;
 void* sub_node_tracker;
 void* current_main_node_map;
 void* current_sub_node_map;
+int main_node_map_counter = 0;
+void* main_node_maps[MAX_SIZE];
+int sub_node_map_counter = 0;
+void* sub_node_maps[MAX_SIZE];
 
 void init_free_list() {
     main_node_tracker = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
@@ -66,6 +71,10 @@ struct main_node* add_main_node() {
     if (main_node_tracker + sizeof(struct main_node) > current_main_node_map + PAGE_SIZE) {
         current_main_node_map = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
                                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+
+        main_node_maps[main_node_map_counter] = current_main_node_map;
+        main_node_map_counter += 1;
         main_node_tracker = current_main_node_map;
         return (struct main_node*)current_main_node_map;
     // else use the current mmap page
@@ -81,6 +90,8 @@ struct sub_node* add_sub_node() {
         current_sub_node_map + PAGE_SIZE) {
         current_sub_node_map = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
                                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        sub_node_maps[sub_node_map_counter] = current_sub_node_map;
+        sub_node_map_counter += 1;
         sub_node_tracker = current_sub_node_map;
         return (struct sub_node*)current_sub_node_map;
     } else {
@@ -122,7 +133,7 @@ void mems_init() {
     head_main->sub_head = NULL;
     start_virtual_address = (void *)1000;
     head_main->v_addr_start = start_virtual_address;
-    head_main->v_addr_end = start_virtual_address;
+    head_main->v_addr_end = start_virtual_address-1;
 }
 
 /*
@@ -130,7 +141,24 @@ This function will be called at the end of the MeMS system and its main job is
 to unmap the allocated memory using the munmap system call. Input Parameter:
 Nothing Returns: Nothing
 */
-void mems_finish() {}
+void mems_finish() {
+    struct main_node* current_main_node = head_main->next;
+    while (current_main_node != head_main) {
+        struct main_node* temp = current_main_node;
+        current_main_node = current_main_node->next;
+        munmap(temp->p_addr, temp->num_of_pages * PAGE_SIZE);
+        struct sub_node* current_sub_node = temp->sub_head;
+        munmap(temp, sizeof(struct main_node));
+    }
+    for (int i = 0; i < main_node_map_counter; i++) {
+        munmap(main_node_maps[i], PAGE_SIZE);
+    }
+    for (int i = 0; i < sub_node_map_counter; i++) {
+        munmap(sub_node_maps[i], PAGE_SIZE);
+    }
+    munmap(main_node_tracker, PAGE_SIZE);
+    munmap(sub_node_tracker, PAGE_SIZE);
+}
 
 /*
 Allocates memory of the specified size by reusing a segment from the free list
@@ -168,7 +196,7 @@ void* mems_malloc(size_t size) {
                     new_sub_node->size = current_sub_node->size - (int)size;
                     new_sub_node->p_addr = (void*)(current_sub_node->p_addr + size);
                     new_sub_node->v_addr_start = (void*)(current_sub_node->v_addr_start + size);
-                    new_sub_node->v_addr_end = current_sub_node->v_addr_end - 1;
+                    new_sub_node->v_addr_end = current_sub_node->v_addr_end;
                     new_sub_node->next = current_sub_node->next;
                     new_sub_node->prev = current_sub_node;
                     if (current_sub_node->next != NULL) {
@@ -201,9 +229,9 @@ void* mems_malloc(size_t size) {
         // (struct main_node*)malloc(sizeof(struct main_node));
     new_main_node->p_addr = p_addr;
     new_main_node->num_of_pages = num_of_pages;
-    new_main_node->v_addr_start = current_main_node->v_addr_end;
+    new_main_node->v_addr_start = current_main_node->v_addr_end + 1;
     // void* temp = current_main_node->v_addr_end + size;
-    new_main_node->v_addr_end = current_main_node->v_addr_end + (num_of_pages * PAGE_SIZE) - 1;
+    new_main_node->v_addr_end = new_main_node->v_addr_start + (num_of_pages * PAGE_SIZE) - 1;
     new_main_node->next = head_main;
     new_main_node->prev = current_main_node;
     current_main_node->next = new_main_node;
@@ -258,28 +286,36 @@ void mems_print_stats() {
     int total_used_pages = 0;
     int total_unused_size = 0;
     int main_chain_length = 0;
-    int sub_chain_lengths[SIZE_MAX];
+    int sub_chain_lengths[MAX_SIZE];
     while (current_main_node != head_main) {
-        printf("MAIN[%d:%d]-> ", current_main_node->v_addr_start, current_main_node->v_addr_end);
+        int sub_chain_length = 0; 
+        printf("MAIN[%lu:%lu]-> ", (uintptr_t)current_main_node->v_addr_start, (uintptr_t)current_main_node->v_addr_end);
         main_chain_length += 1;
         struct sub_node* current_sub_node = current_main_node->sub_head;
         int main_node_pages = current_main_node->num_of_pages;
         while (current_sub_node != NULL) {
+            sub_chain_length += 1;
             if (current_sub_node->type == HOLE) {
-                printf("H[%d:%d] <-> ", current_sub_node->v_addr_start, current_sub_node->v_addr_end);
+                printf("H[%lu:%lu] <-> ", (uintptr_t)current_sub_node->v_addr_start, (uintptr_t)current_sub_node->v_addr_end);
                 total_unused_size += current_sub_node->size;
             } else {
-                printf("P[%d:%d] <->", current_sub_node->v_addr_start, current_sub_node->v_addr_end);
+                printf("P[%lu:%lu] <-> ", (uintptr_t)current_sub_node->v_addr_start, (uintptr_t)current_sub_node->v_addr_end);
                 total_used_pages += 1;
             }
             current_sub_node = current_sub_node->next;
         }
+        sub_chain_lengths[main_chain_length - 1] = sub_chain_length;
         current_main_node = current_main_node->next;
         printf("NULL\n");
     }
     printf("Total used pages: %d\n", total_used_pages);
     printf("Total unused size: %d\n", total_unused_size);
     printf("Main chain length: %d\n", main_chain_length);
+    printf("Sub chain length array: [");
+    for (int i = 0; i < main_chain_length; i++) {
+         printf("%d, ", sub_chain_lengths[i]);
+    }
+    printf("]\n");  
 }
 
 /*
@@ -294,8 +330,7 @@ void* mems_get(void* v_ptr) {
         struct sub_node* current_sub_node = current_main_node->sub_head;
         while (current_sub_node != NULL) {
             // if the v_ptr is in the sub_node
-            if (v_ptr >= current_sub_node->v_addr_start &&
-                v_ptr < current_sub_node->v_addr_end && current_sub_node->type == PROCESS) {
+            if (v_ptr >= current_sub_node->v_addr_start && v_ptr < current_sub_node->v_addr_end && current_sub_node->type == PROCESS) {
                 return current_sub_node->p_addr + (v_ptr - current_sub_node->v_addr_start);
             }
             current_sub_node = current_sub_node->next;
@@ -310,6 +345,59 @@ this function free up the memory pointed by our virtual_address and add it to
 the free list Parameter: MeMS Virtual p_addr (that is created by MeMS) Returns:
 nothing
 */
-void mems_free(void* v_ptr) {
 
+void merge_holes() {
+    struct main_node* current_main_node = head_main->next;
+    while (current_main_node != head_main) {
+        struct sub_node* current_sub_node = current_main_node->sub_head;
+        while (current_sub_node != NULL) {
+            // if the current sub_node is HOLE
+            if (current_sub_node->type == HOLE) {
+                // if the next sub_node is HOLE, merge them
+                if (current_sub_node->next != NULL && current_sub_node->next->type == HOLE) {
+                    current_sub_node->size += current_sub_node->next->size;
+                    current_sub_node->v_addr_end = current_sub_node->next->v_addr_end;
+                    current_sub_node->next = current_sub_node->next->next;
+                    if (current_sub_node->next != NULL) {
+                        current_sub_node->next->prev = current_sub_node;
+                    }
+                }
+                // if the prev sub_node is HOLE, merge them
+                if (current_sub_node->prev != NULL && current_sub_node->prev->type == HOLE && current_sub_node->prev != current_sub_node) {
+                    current_sub_node->prev->size += current_sub_node->size;
+                    current_sub_node->prev->v_addr_end = current_sub_node->v_addr_end;
+                    current_sub_node->prev->next = current_sub_node->next;
+                    if (current_sub_node->next != NULL) {
+                        current_sub_node->next->prev = current_sub_node->prev;
+                    }
+                }
+            }
+            current_sub_node = current_sub_node->next;
+        }
+        current_main_node = current_main_node->next;
+    }
+
+}
+
+void mems_free(void* v_ptr) {
+    // traverse through the free list
+    struct main_node* current_main_node = head_main->next;
+    while (current_main_node != head_main) {
+        struct sub_node* current_sub_node = current_main_node->sub_head;
+        while (current_sub_node != NULL) {
+            // if the v_ptr is in the sub_node
+            if (v_ptr >= current_sub_node->v_addr_start && v_ptr < current_sub_node->v_addr_end && current_sub_node->type == PROCESS) {
+                // make the sub_node HOLE
+                current_sub_node->type = HOLE;
+                // merge the holes
+                merge_holes();
+                return;
+            }
+            current_sub_node = current_sub_node->next;
+        }
+        current_main_node = current_main_node->next;
+    }
+    // if no such process found
+    printf("No such process found\n");
+    return;
 }
