3f5791bfe0c380417724fc2bd0720cc0637a3f60
0xGodspeed <74945202+0xGodspeed@users.noreply.github.com>
2023-11-15 03:50:38 +0530

    q2 done (v1)

--- ass4/README.md
+++ ass4/README.md
@@ -16,3 +16,23 @@
 ## Implementation details
 - Bowls have been implemented using a single conditional variable which signals/waits for any bowl that is available. An integer array is used to keep track of which bowls are available and which bowl is held by which philosopher. Each philosopher loops through the array and checks if a bowl is available; if not it waits for the conditional variable to be signalled. When a philosopher is done eating, it puts down both forks and bowls (the array is updated) and signals the conditional variable. After the conditional variable is signalled and the code resumes, we loop through the array again to check which bowl was made available and update the array again.
 - For forks, there is a conditional variable for each fork. Each philosopher waits for its respective left and right fork (in that order). The conditional variable of the respective fork is signaled when the fork is put down. 
+
+# Q2 - Car and passengers
+
+## Implementation details
+- the loop goes on till all passengers have been served.
+#### Globals
+- `capacity`: the number of passengers that can be loaded into the car at a time
+- `total_passengers`: the total number of passengers
+- `passengers_on_board`: the number of passengers on board at a given time
+
+#### Semaphores
+- there are 4 semaphores being used - `ride`, `carLoaded`, `carUnloaded`, `boardMutex`
+  - `carLoaded`: is used to signal passengers that the car has been loaded. Each passenger initially waits on this semaphore. When the car is loaded, the car thread signals this `capacity` number of times to signal each passenger and to let each passenger thread now that the car has been loaded.
+  -  `boardMutex`: is a binary semaphore that is used to change `passengers_on_board` atomically. It is used to decrease/increase the number of passengers on board.
+  -  `ride`: is used to signal the passengers that the ride has completed. Each passenger waits on this semaphore. When the ride is completed, the car thread signals this `capacity` number of times to signal each passenger and to let each passenger thread now that the ride has completed.
+  -  `carUnloaded`: is used to signal the passengers that the car has been unloaded. Each passenger waits on this semaphore. When the car is unloaded, the car thread signals this `capacity` number of times to signal each passenger and to let each passenger thread now that the car has been unloaded.
+
+  ![](q2_flow.png)
+
+# Q3
---
+++ ass4/q2.c
@@ -0,0 +1,149 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define TIME 2 * 1000000
+
+int total_passengers;
+int capacity;
+int passengers_on_board = 0;
+
+sem_t ride;
+sem_t carLoaded, carUnloaded;
+sem_t boardMutex;
+
+void load() { usleep(random() % TIME); }
+
+void unload() { usleep(random() % TIME); }
+
+void ride_car() {
+    usleep(random() % TIME);
+    printf("Car ride done.\n");
+}
+
+void board(int passenger_id) {
+    printf("Passenger %d boards the car.\n", passenger_id);
+    usleep(random() % TIME);
+}
+
+void offboard(int passenger_id) {
+    printf("Passenger %d gets off the car.\n", passenger_id);
+    usleep(random() % TIME);
+}
+
+void* passenger(void* args) {
+    int passenger_id = *((int*)args);
+    printf("Passenger %d is waiting for car to be loaded.\n", passenger_id);
+    sem_wait(&carLoaded);
+    printf("Passenger %d knows that car is loaded.\n", passenger_id);
+
+    // while (passengers_on_board < capacity) {
+    sem_wait(&boardMutex);
+    passengers_on_board++;
+    board(passenger_id);
+    printf("Passenger %d is on board.\n", passenger_id);
+    sem_post(&boardMutex);
+
+    sem_wait(&ride);
+    printf("Passenger %d knows that ride is done.\n", passenger_id);
+    // }
+
+    sem_wait(&carUnloaded);
+    printf("Passenger %d knows that car is unloaded.\n", passenger_id);
+
+    sem_wait(&boardMutex);
+    offboard(passenger_id);
+    printf("Passenger %d is off board.\n", passenger_id);
+    passengers_on_board--;
+    sem_post(&boardMutex);
+}
+
+void* car(void* args) {
+    while (total_passengers > 0) {
+        load();
+        // let all passenger threads know that car is loaded
+        for (int i = 0; i < capacity; i++) {
+            sem_post(&carLoaded);
+        }
+        printf("Car is loaded.\n");
+
+        while (1) {
+            sem_wait(&boardMutex);
+            //  for when the remaining passengers are less than capacity
+            if (passengers_on_board ==
+                (total_passengers < capacity ? total_passengers : capacity)) {
+                sem_post(&boardMutex);
+                break;
+            }
+            sem_post(&boardMutex);
+        }
+
+        printf("Car is full with %d passengers.\n", passengers_on_board);
+
+        ride_car();
+
+        // all passenger threads are waiting for ride semaphore
+        // let them know that ride is done
+        for (int i = 0; i < passengers_on_board; i++) {
+            sem_post(&ride);
+        }
+
+        printf("Car ride done.\n");
+
+        unload();
+
+        // let all passenger threads know that car is unloaded
+        for (int i = 0; i < capacity; i++) {
+            sem_post(&carUnloaded);
+        }
+        printf("Car is unloaded.\n");
+
+        while (1) {
+            sem_wait(&boardMutex);
+            if (passengers_on_board == 0) {
+                sem_post(&boardMutex);
+                break;
+            }
+            sem_post(&boardMutex);
+        }
+
+        printf("Car is empty with %d passengers.\n", passengers_on_board);
+        total_passengers -= capacity;
+    }
+    printf("All passengers have ridden the car.\n");
+}
+
+void init() {
+    sem_init(&ride, 0, 0);
+    sem_init(&carLoaded, 0, 0);
+    sem_init(&carUnloaded, 0, 0);
+    sem_init(&boardMutex, 0, 1);
+}
+
+int main() {
+    printf("Enter total number of passengers: ");
+    scanf("%d", &total_passengers);
+    printf("Enter capacity of car: ");
+    scanf("%d", &capacity);
+    init();
+    pthread_t passengers[total_passengers];
+    pthread_t carThread;
+    int passenger_id[total_passengers];
+    int car_id = 0;
+    pthread_create(&carThread, NULL, car, &car_id);
+    for (int i = 0; i < total_passengers; i++) {
+        passenger_id[i] = i;
+        pthread_create(&passengers[i], NULL, passenger, &passenger_id[i]);
+    }
+    for (int i = 0; i < total_passengers; i++) {
+        pthread_join(passengers[i], NULL);
+    }
+    if (pthread_join(carThread, NULL) != 0) {
+        printf("Thread join failed\n");
+        return 1;
+    }
+    return 0;
+}
